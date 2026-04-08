f21d39426a629ec2c6136f865b41aa9cdcf92304
ariyasthenics <email@thekillercorona@gmail.com>
2026-04-08 10:25:47 +0700

    anajy

--- MahasiswaBerprestasi.java
+++ MahasiswaBerprestasi.java
@@ -51,4 +51,50 @@ public class MahasiswaBerprestasi {
             listMhs[j]=temp;
         }
     }
+    int sequentialSerarching(double cari) {
+        int posisi = -1;
+        for (int j=0; j<listMhs.length; j++){
+            if (listMhs[j].ipk==cari) {
+                posisi=j;
+                break;
+            }
+        }
+        return posisi;
+    }
+    void tampilPosisi(double x, int pos){
+        if (pos!=-1) {
+            System.out.println("data mahasiswa dengan ipk :" +x+ "ditemukan indeks " +pos );
+        }
+        else {
+            System.out.println("data " +x+ "tidak di temukan");
+        }
+    }
+    void tampilDataSearch(double x, int pos) {
+        if (pos !=-1) {
+            System.out.println("nim\t : "+listMhs[pos].nim);
+            System.out.println("nama\t : "+listMhs[pos].nama);
+            System.out.println("kelas\t : "+listMhs[pos].kelas);
+            System.out.println("ipk\t : "+x);
+        }
+        else{
+            System.out.println("data mahasiswa dengan ipk " +x+ "tidak ditemukan ");
+        }
+    }
+    int findBinarySearch(double cari, int left, int right) {
+        int mid;
+        if (right>=left) {
+            mid =(left+right)/2;
+            if (cari==listMhs[mid].ipk) {
+                return (mid);
+            } 
+            else if (listMhs[mid].ipk<cari) {
+                return findBinarySearch(cari, left, mid-1);
+            }
+            else {
+                return findBinarySearch(cari, mid+1, right);
+            }
+        }
+        return -1;
+
+    }
 }
--- MahasiswaDemo16.java
+++ MahasiswaDemo16.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
+
 public class MahasiswaDemo16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MahasiswaBerprestasi list = new MahasiswaBerprestasi();
-
+        
         System.out.print("Masukkan jumlah mahasiswa: ");
-        int n = sc.nextInt();
-        sc.nextLine(); // buang enter
-
-        for (int i = 0; i < n; i++) {
+        int jumMhs = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < jumMhs; i++) {
             System.out.println("Data mahasiswa ke-" + (i+1));
             System.out.print("NIM: ");
             String nim = sc.nextLine();
@@ -18,24 +18,25 @@ public class MahasiswaDemo16 {
             String kelas = sc.nextLine();
             System.out.print("IPK: ");
             double ipk = sc.nextDouble();
-            sc.nextLine(); // buang enter
-            Mahasiswa16 m = new Mahasiswa16(nim, nama, kelas, ipk);
-            list.tambah(m);
+            sc.nextLine();
+            System.out.println("-------------------------------");
+            list.tambah(new Mahasiswa16(nim, nama, kelas, ipk));
         }
-        System.out.println("Data sebelum sorting:");
+
         list.tampil();
 
-        list.bubleSort();
-
-        System.out.println("Data setelah sorting (desc):");
-        list.tampil();
-
-        System.out.println("data yang sudah terurut menggunakan Slection sort (ASC) ");
-        list.SelectionSort();
-        list.tampil();
-        System.out.println("data yang sudah ter urut menggunakan insertion sort (ASC)");
+        System.out.println("=================================");
+        System.out.println("Pencarian data");
+        System.out.println("=================================");
+        System.out.print("Masukkan IPK yang dicari: ");
+        double cari = sc.nextDouble();
+        System.out.println("=================================");
+        System.out.println("Menggunakan Binary search");
+        System.out.println("=================================");
         list.insertionSort();
-        list.tampil();
-
+        double posisi2 = list.findBinarySearch(cari,0, jumMhs-1);
+        int pss2= (int)posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
     }
 }
